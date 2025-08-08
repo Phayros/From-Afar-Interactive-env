f097d0fa4de98ca91546ece3755970c154808c4f
Phayros <phayrosonly@gmail.com>
2025-08-08 20:08:48 -0300

    :construction: Added new data folder and placeholder map and changed where the background image placeholder was stored

---
+++ data/tilesets/backgound placeholder.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="backgound placeholder" tilewidth="7680" tileheight="4320" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image source="../graphics/bg-ph.png" width="7680" height="4320"/>
+ </tile>
+</tileset>
