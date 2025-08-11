531e7a4351639d96a2115d8a684e024cf0921f7d
Phayros <phayrosonly@gmail.com>
2025-08-11 00:19:14 -0300

    :lipstick: feat: Change the background placeholder to a green tone

--- data/tilesets/backgound placeholder.tsx
+++ data/tilesets/backgound placeholder.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="backgound placeholder" tilewidth="7680" tileheight="4320" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="backgound placeholder" tilewidth="7680" tileheight="4320" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="4">
   <image source="../graphics/bg-ph.png" width="7680" height="4320"/>
+ </tile>
+ <tile id="5">
+  <image source="../graphics/bg-ph-2.png" width="7680" height="4320"/>
  </tile>
 </tileset>
